79e4741034c0d32d5046f1b0a63097ed95b0a61c
almazbekovvass <almazbekkayperi>
2024-02-06 15:37:04 +0600

    DZ8

--- DZ8_Aiperi_34-03/app.js
+++ DZ8_Aiperi_34-03/app.js
@@ -1,36 +1,49 @@
-const buttons = document.querySelectorAll('button')
-const modalList = document.querySelector('.modal_list')
-const wrapper = document.querySelector('.wrapper')
-const column = document.querySelectorAll('.column')
-const  createWindow = () => {
-    const darkBg = document.createElement('div')
-    darkBg.setAttribute('class', 'darkBg')
-    const modalWindow = document.createElement("div")
-    modalWindow.setAttribute('class','modalWindow')
-    const textModalWindow = document.createElement('div')
-    textModalWindow.setAttribute('class','textModalWindow')
-    textModalWindow.innerHTML = 'Подтверждение'
-    const modalWindowButton = document.createElement('button')
-    modalWindowButton.setAttribute('class','modalWindowButton')
-    modalWindowButton.innerHTML = 'OK'
-    modalWindow.append(textModalWindow, modalWindowButton)
-    darkBg.append(modalWindow)
-    modalList.append(darkBg)
+let movies = [
+    {title: 'spider man', year: 2020, rating: '7.7', director: 'dj frow'},
+    {title: 'Отель', year: 2024, rating: '8.9', director: 'Орозгуль Абдыкадырова'},
+    {title: 'Оно', year: 2018, rating: '3.5', director: 'Ф.В Вилли'},
+    {title: 'Паразиты', year: 2017, rating: '9.7', director: 'Ди Джи'}
+]
 
-    modalWindowButton.onclick=() => {
-        darkBg.style.display='none'
-        this.column.style.background= 'silver'
-        this.buttons.style.display='none'
-    }
+let table = document.getElementById('movieTable')
 
-
-}
-buttons.forEach ((button) => {
-    button.onclick=()=> {
-        createWindow()
-    }
-
+movies.forEach((movie, index) =>{
+    let row = table.insertRow(-1)
+    row.insertCell(0).innerText = movie.title
+    row.insertCell(1).innerText = movie.year
+    row.insertCell(2).innerText = movie.rating
+    row.insertCell(3).innerText = movie.director
+    let buyButton = row.insertCell(4)
+    buyButton.innerHTML = '<button class="buyButton">Купить</button>'
 })
+
+let modal = document.getElementById('myModal')
+let span = document.getElementsByClassName("close")[0]
+let confirmPurchase = document.getElementById('btn')
+let selectedCell
+
+table.addEventListener('click', (e) =>{
+    if (e.target && e.target.className === 'buyButton') {
+        modal.style.display = "block"
+        selectedCell = e.target.parentElement
+    }
+})
+
+span.onclick = ()=> {
+    modal.style.display = "none"
+}
+
+confirmPurchase.onclick = () => {
+    selectedCell.classList.add('background')
+    selectedCell.innerHTML = ''
+    modal.style.display = "none";
+}
+
+window.onclick = (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
 
 
 
